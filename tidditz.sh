e0d53e6569c7e6be4dcee3c197b97bf8ffd90207
Emine190 <117157285+Emine190@users.noreply.github.com>
2022-11-18 14:27:58 +0100

    Update tidditz.sh

--- tidditz.sh
+++ tidditz.sh
@@ -1,16 +1,16 @@
 #### References and variants #####
-ref_GATK=/media/user/redacted/skewing_project/indexes_annotations/bwa/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
+ref_GATK=/media/user/redacted/indexes_annotations/bwa/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
 
 
 
 cat /media/user/redacted/wgs/runfile.txt | while read participant    BAM_file
 do
-tiddit --sv --threads 100 -o /media/user/redacted/wgs/tiddit_out/$participant. --bam /media/colne37/platypus/wgs/$BAM_file --ref $ref_GATK
+tiddit --sv --threads 100 -o /media/user/redacted/wgs/tiddit_out/$participant. --bam /media/user/redacted/wgs/$BAM_file --ref $ref_GATK
 done
 
 
 
 cat /media/user/redacted/wgs/runfile.txt | while read participant    BAM_file
 do
-tiddit --cov -o /media/user/redacted/wgs/tiddit_out/cov/$participant'_'cov. --bam /media/colne37/platypus/wgs/$BAM_file
+tiddit --cov -o /media/user/redacted/wgs/tiddit_out/cov/$participant'_'cov. --bam /media/user/redacted/wgs/$BAM_file
 done
