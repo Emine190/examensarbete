d9207778f3fe4eee60b4bace142123660da02a3b
Emine190 <117157285+Emine190@users.noreply.github.com>
2022-11-18 08:46:35 +0100

    Update filter.sh

--- filter.sh
+++ filter.sh
@@ -1,18 +1,25 @@
-gatk=/media/god/data1/software/gatk-4.2.6.1/gatk
+gatk=/media/software/gatk-4.2.6.1/gatk
 
 #$gatk SelectVariants \
 #--variant ApplyVSQR.recalibrated.vcf \
-#--output 13PLJ_filtered_ApplyVSQR.recalibrated.vcf \
+#--output Woman2_filtered_ApplyVSQR.recalibrated.vcf \
 #--exclude-filtered TRUE \
 #--restrict-alleles-to BIALLELIC \
-#-sn GTEX-13PLJ-0003-SM-6WSSCN
+#-sn Woman2
 
 #$gatk SelectVariants \
 #--variant ApplyVSQR.recalibrated.vcf \
-#--output UPIC_filtered_ApplyVSQR.recalibrated.vcf \
+#--output Woman1_filtered_ApplyVSQR.recalibrated.vcf \
 #--exclude-filtered TRUE \
 #--restrict-alleles-to BIALLELIC \
-#-sn GTEX-UPIC-0004-SM-5SOEF
+#-sn Woman1
+
+#$gatk SelectVariants \
+#--variant ApplyVSQR.recalibrated.vcf \
+#--output Woman3_filtered_ApplyVSQR.recalibrated.vcf \
+#--exclude-filtered TRUE \
+#--restrict-alleles-to BIALLELIC \
+#-sn Woman3
 
 #$gatk SelectVariants \
 #--variant ApplyVSQRControl.recalibrated.vcf \
@@ -20,17 +27,19 @@ gatk=/media/god/data1/software/gatk-4.2.6.1/gatk
 #--exclude-filtered TRUE \
 #--restrict-alleles-to BIALLELIC
 
-
+# applies a filter to the samples and control so only the SNPs and Indels that are expressed on one 1 X-chr
 #bcftools filter -i 'GT="het"' Control_filtered_ApplyVSQR.recalibrated.vcf > het_Control.vcf
 
-#bcftools filter -i 'GT="het"' UPIC_filtered_ApplyVSQR.recalibrated.vcf > het_UPIC.vcf
+#bcftools filter -i 'GT="het"' Woman1_filtered_ApplyVSQR.recalibrated.vcf > het_Woman1.vcf
 
-#bcftools filter -i 'GT="het"' ZZPU_filtered_ApplyVSQR.recalibrated.vcf > het_ZZPU.vcf
+#bcftools filter -i 'GT="het"' Woman2_filtered_ApplyVSQR.recalibrated.vcf > het_Woman2.vcf
 
+#bcftools filter -i 'GT="het"' Woman3_filtered_ApplyVSQR.recalibrated.vcf > het_Woman3.vcf
 
-#bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%AD\t%SAMPLE]\n' het_13PLJ.vcf > het_13PLJ.bed
-#bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%AD\t%SAMPLE]\n' het_UPIC.vcf > het_UPIC.bed
-#bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%AD\t%SAMPLE]\n' het_ZZPU.vcf > het_ZZPU.bed
+#Switch to bed format for easier handling in R
+#bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%AD\t%SAMPLE]\n' het_Woman1.vcf > het_Woman1.bed
+#bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%AD\t%SAMPLE]\n' het_Woman2.vcf > het_Woman2.bed
+#bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%AD\t%SAMPLE]\n' het_Woman3vcf > het_Woman3.bed
 
 bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%AD\t%SAMPLE]\n' het_Control.vcf > het_Control.bed
 
